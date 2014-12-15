1fac5c21da4149082f123e19878a7b2eb0c3d1cb
Sadık Faruk Çetin <sadik.cetin34@gmail.com>
2014-12-15 23:05:18 +0200

    refs#67 sessions controller created

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,11 @@
 Rails.application.routes.draw do
 
+  get 'sessions/new'
+
+  get 'sessions/create'
+
+  get 'sessions/destroy'
+
   get '/:id', to: 'users#show',as: :profile
   get '/users/new',to: redirect('/kaydol')
   resources :users,except: :index
