ee98efef20a61a4c279664addc690bdb7aab6f87
Sadık Faruk Çetin <sadik.cetin34@gmail.com>
2014-12-15 23:03:00 +0200

    profile page created

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,9 @@
 class User < ActiveRecord::Base
+  has_secure_password
+  validates :password ,presence:  true,length: {minimum: 6}
+
+  def avatar_url
+    hash_value =Digest::MD5.hexdigest(email.downcase)
+    "http://www.gravatar.com/avatar/#{hash_value}?s=160"
+  end
 end
